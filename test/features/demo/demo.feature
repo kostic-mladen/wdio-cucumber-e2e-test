9fb7da055db4d8f62e0a22f3e8d1015f9b1abb06
Mladen <kostic.mladen@outlook.com>
2026-02-22 18:43:16 +0100

    Web Interactions

--- test/features/demo/demo.feature
+++ test/features/demo/demo.feature
@@ -1,10 +1,12 @@
-Feature: Feature name
+Feature: Demo Feature
 
-    Feature Description
-    This is the demo Feature
-    @demo
-    Scenario: Navigate to the login screen
+   Scenario Outline: Run first demo feature
 
-          Given the user lanches the CURA web app
-          When the user clicks on the Make Appointment button
-          Then the login page should be displayed
+          Given The user wants to login
+          When The user enters the username and password
+          Then The user should be logged in the application
+
+            Examples:
+                | TestID    | Username      | Password     |
+                | DEMO_TC001| standard_user | secret_sauce | 
+                
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,14 +1,49 @@
 import {Given, When, Then} from "@wdio/cucumber-framework";
 
-Given(/^the user lanches the CURA web app$/, async function(){
-    await browser.url("https://katalon-demo-cura.herokuapp.com/");
-    await expect(browser).toHaveTitle("CURA Healthcare Service");
+Given(/^The user wants to login$/, async function() {
+    await browser.maximizeWindow()
+    await browser.setTimeout({ implicit: 5000, pageLoad: 10000  })
+    await browser.url('https://www.saucedemo.com/');
 });
 
-When(/^the user clicks on the Make Appointment button$/, async function(){
-    await $("=Make Appointment").click();
+When(/^The user enters the username and password$/, async function() {
+    
+    const usernameInput = await $('#user-name');
+    const passwordInput = await $('#password');  
+
+    // Enter the credentials
+    await usernameInput.setValue('standard_user');
+    await passwordInput.setValue('secret_sauce');
+
+    
+    const loginButton = await $('#login-button');
+    await loginButton.click();
 });
 
-Then(/^the login page should be displayed$/, async function() {
-   await expect(browser).toHaveUrl(/.*#login/);
-}); 
+Then(/^The user should be logged in the application$/, async function() {
+    const inventoryPage = await $('#inventory_container'); 
+    await expect(inventoryPage).toBeDisplayed();
+    });
+
+Given(/^A web page is opened$/, async function() {
+    await browser.url("/inputs")
+    await browser.setTimeout({ implicit: 5000, pageLoad: 10000  })
+    await browser.maximizeWindow()
+})
+
+When(/^Perform web interactions$/, async function() {
+   let num = 12345
+   let strNum = num.toString()
+   
+    let inputField = await $(`[type=number]`)
+   // await inputField.setValue(strNum)
+   await inputField.click()
+    
+    for(let i=0; i<strNum.length; i++){
+        let chatStr = strNum.charAt(i)
+        await browser.pause(1000)
+        await browser.keys(chatStr)
+    }
+
+    await browser.pause(3000)
+    })
